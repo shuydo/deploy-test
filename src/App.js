65d75578b97f701a0f77ea0401bf7c212513374a
Shuydo <shuydo12@gmail.com>
2023-03-08 02:17:10 +0200

    4add

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reloADD.
+          Edit <code>src/App.js</code> and save to relo4ad.
         </p>
         <a
           className="App-link"
